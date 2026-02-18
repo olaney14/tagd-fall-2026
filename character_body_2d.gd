9bdaace5242c314ec499a241654f201e98a5ed30
airvick777 <alberik300@gmail.com>
2026-02-17 20:55:44 -0600

    Additions
    Added a interact(kinda) added some sprites to test but have not finished implementing. I also changed input names and made jump into spacebar

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -11,10 +11,12 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y += jump_velocity
 	
-	var direction := Input.get_axis("move_left", "move_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+	if Input.is_action_just_pressed("interact"):
+		print("BAMMM!!!!!!!!")
 
 	move_and_slide()
