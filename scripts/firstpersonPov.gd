87bd323f2e546bec0044a209276dc1b75736e1d5
airvick777 <alberik300@gmail.com>
2026-02-18 13:41:16 -0600

    MUAHAHAHA(eleveator and 1st person cam)
    I made the elevetor(still work in progress) and the camera movement for the 1st person POV

---
+++ scripts/firstpersonPov.gd
@@ -0,0 +1,25 @@
+extends Node3D
+var edge_margin = 50 # Pixels from edge to trigger rotation
+var senstivity = 0.01
+@onready var viewport = get_viewport()
+@onready var head = $"."
+@onready var camera = $camera
+
+func _process(delta):
+	var mouse_pos = viewport.get_mouse_position()
+	var viewport_size = viewport.size
+	
+	# Rotate right
+	if mouse_pos.x > viewport_size.x - edge_margin:
+		head.rotate_y(-senstivity)
+	# Rotate left
+	elif mouse_pos.x < edge_margin:
+		head.rotate_y(senstivity)
+		
+	# Vertical rotation (optional)
+	if mouse_pos.y > viewport_size.y - edge_margin:
+		camera.rotate_x(-senstivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+	elif mouse_pos.y < edge_margin:
+		camera.rotate_x(senstivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
