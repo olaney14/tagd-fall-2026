a8ab44b825a587e5e2a340b50f49afa177e1d3e4
airvick777 <alberik300@gmail.com>
2026-03-23 14:27:03 -0500

    trying to ray cacast
    it not work but i have a mouse button click detection, changed camera movement in elevator to wasd, a collision added but its not detected.

--- scripts/firstpersonPov.gd
+++ scripts/firstpersonPov.gd
@@ -1,25 +1,51 @@
 extends Node3D
 var edge_margin = 100 # Pixels from edge to trigger rotation
-var senstivity = 0.01
+var senstivity = 0.008
 @onready var viewport = get_viewport()
 @onready var head = $"."
 @onready var camera = $Camera
-
+@onready var interaction_ray = $RayCast3D
 func _process(delta):
-	var mouse_pos = viewport.get_mouse_position()
-	var viewport_size = viewport.size
-	
 	# Rotate right
-	if mouse_pos.x > viewport_size.x - edge_margin:
+	if Input.is_action_pressed("right"):
 		head.rotate_y(-senstivity)
 	# Rotate left
-	elif mouse_pos.x < edge_margin:
+	elif Input.is_action_pressed("left"):
 		head.rotate_y(senstivity)
 		
 	# Vertical rotation (optional)
-	if mouse_pos.y > viewport_size.y - edge_margin:
+	if Input.is_action_pressed("down"):
 		camera.rotate_x(-senstivity)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
-	elif mouse_pos.y < edge_margin:
+	elif Input.is_action_pressed("up"):
 		camera.rotate_x(senstivity)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		
+
+	
+	#var mouse_pos = viewport.get_mouse_position()
+	#var viewport_size = viewport.size
+	#
+	## Rotate right
+	#if mouse_pos.x > viewport_size.x - edge_margin:
+		#head.rotate_y(-senstivity)
+	## Rotate left
+	#elif mouse_pos.x < edge_margin:
+		#head.rotate_y(senstivity)
+		#
+	## Vertical rotation (optional)
+	#if mouse_pos.y > viewport_size.y - edge_margin:
+		#camera.rotate_x(-senstivity)
+		#camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+	#elif mouse_pos.y < edge_margin:
+		#camera.rotate_x(senstivity)
+		#camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if interaction_ray.is_colliding():
+			print_debug("The pew pew reached")
+			var collider = interaction_ray.get_collider()	
+		print("press pew pew")
+func _unhandled_input(event):
+	pass
